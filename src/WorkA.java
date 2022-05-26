8fba289eca555d94dfa5851af3dec6e3cf98c5a6
AkhtamIsPutin <Kuukilovekulpush@gmail.com>
2022-05-26 16:08:50 +0600

    HW2_2

---
+++ src/WorkA.java
@@ -0,0 +1,92 @@
+public class WorkA {
+}
+
+class Animal {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class popugay {
+    private String color;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public popugay(String color) {
+        this.color = color;
+    }
+}
+
+class dog {
+    private String poroda;
+
+    public String getPoroda() {
+        return poroda;
+    }
+
+    public void setPoroda(String poroda) {
+        this.poroda = poroda;
+    }
+
+    public dog(String poroda) {
+        this.poroda = poroda;
+    }
+}
+
+class cat {
+    private String ushki;
+
+    public String getUshki() {
+        return ushki;
+    }
+
+    public void setUshki(String ushki) {
+        this.ushki = ushki;
+    }
+
+    public cat(String ushki) {
+        this.ushki = ushki;
+    }
+}
+
+class fish {
+    private int kolichestvo;
+
+    public int getKolichestvo() {
+        return kolichestvo;
+    }
+
+    public void setKolichestvo(int kolichestvo) {
+        this.kolichestvo = kolichestvo;
+    }
+
+    public fish(int kolichestvo) {
+        this.kolichestvo = kolichestvo;
+    }
+}
